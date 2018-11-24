b109ab04f5081e7dc62ced54a9837390cdae4f9e
Jon Oakeson <38961891+Alphakip@users.noreply.github.com>
2018-11-24 09:18:22 -0700

    ok

--- Items/Accessories/TimeStone.cs
+++ Items/Accessories/TimeStone.cs
@@ -1,8 +1,10 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using Terraria;
 using Terraria.DataStructures;
+using Terraria.Graphics.Shaders;
 using Terraria.ID;
 using Terraria.ModLoader;
 
