b36dd3b10a90d4673373b69acfe04556c10a8dde
TailstheFox92 <41931137+TailstheFox92@users.noreply.github.com>
2018-11-25 22:59:04 -0500

    Create EternalTwilight.cs

---
+++ Items/Boss/Yamata/EternalTwilight.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace AncientsAwakened.Items.Boss.Yamata
+{
+    public class EternalTwilight : ModItem 
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Eternal Twilight");
+            Tooltip.SetDefault(""); //TODO: Come up with a good tooltip
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 44;
+            item.height = 76;
+            item.damage = 180;
+            item.useTime = 8;
+            item.useAnimation = 8;
+            item.useStyle = 5;
+            item.value = Item.buyPrice(1, 0, 0, 0);
+            item.UseSound = SoundID.Item20;
+            item.autoReuse = true;
+            item.shootSpeed = 15f;
+            item.knockBack = 5;
+            item.ranged = true;
+            item.noMelee = true;
+        }
+    }
+}
