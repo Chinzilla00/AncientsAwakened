5954548f0399f81f1c9fa99fef5682291ba24812
unknown <leeleegill20@gmail.com>
2018-11-27 01:07:26 -0500

    added yamata spear

---
+++ Items/Boss/Yamata/AbyssalYari.cs
@@ -0,0 +1,41 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AAMod.Items.Boss.Yamata
+{
+    public class AbyssalYari : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Abyssal Yari");
+        }
+
+        public override void SetDefaults()
+        {
+            item.damage = 100; //idk
+            item.melee = true;
+            item.width = 132;
+            item.height = 132;
+            item.scale = 1.1f;
+            item.maxStack = 1;
+            item.useTime = 25; //idk
+            item.useAnimation = 25; //idk
+            item.knockBack = 2f;
+            item.UseSound = SoundID.Item1;
+            item.noMelee = true;
+            item.noUseGraphic = true;
+            item.useStyle = 5;
+            item.value = Item.sellPrice(0, 2, 40, 0); //idk
+            item.rare = 2; //idk
+            item.shootSpeed = 5f;
+            item.shoot = mod.ProjectileType("AbyssalYariP");  
+            item.autoReuse = true;
+        }
+
+        public override bool CanUseItem(Player player)
+        {
+            return player.ownedProjectileCounts[item.shoot] < 1; // This is to ensure the spear doesn't bug out when using autoReuse = true
+        }
+    }
+}
---
+++ Projectiles/Yamata/AbyssalYariP.cs
@@ -0,0 +1,113 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using System;
+
+namespace AAMod.Projectiles.Yamata
+{
+    public class AbyssalYariP : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            projectile.width = 75;
+            projectile.height = 75;
+            projectile.scale = 1.1f;
+            projectile.aiStyle = 19;
+            projectile.friendly = true;
+            projectile.hostile = false;
+            projectile.tileCollide = false;
+            projectile.penetrate = -1;
+            projectile.ownerHitCheck = true;
+            projectile.melee = true;
+            projectile.timeLeft = 90;
+            projectile.hide = true;
+        }
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("AbyssalYariP");
+        }
+
+        public float MovementFactor // Change this value to alter how fast the spear moves
+        {
+            get { return projectile.ai[0]; }
+            set { projectile.ai[0] = value; }
+        }
+
+        public override void AI()
+        {
+            // Since we access the owner player instance so much, it's useful to create a helper local variable for this
+            // Sadly, Projectile/ModProjectile does not have its own
+            Player projOwner = Main.player[projectile.owner];
+            // Here we set some of the projectile's owner properties, such as held item and itemtime, along with projectile direction and position based on the player
+            Vector2 ownerMountedCenter = projOwner.RotatedRelativePoint(projOwner.MountedCenter, true);
+            projectile.direction = projOwner.direction;
+            projOwner.heldProj = projectile.whoAmI;
+            projOwner.itemTime = projOwner.itemAnimation;
+            projectile.position.X = ownerMountedCenter.X - (float)(projectile.width / 2);
+            projectile.position.Y = ownerMountedCenter.Y - (float)(projectile.height / 2);
+            // As long as the player isn't frozen, the spear can move
+            if (!projOwner.frozen)
+            {
+                if (MovementFactor == 0f) // When initially thrown out, the ai0 will be 0f
+                {
+                    MovementFactor = 3f; // Make sure the spear moves forward when initially thrown out
+                    projectile.netUpdate = true; // Make sure to netUpdate this spear
+                }
+                if (projOwner.itemAnimation < projOwner.itemAnimationMax / 3) // Somewhere along the item animation, make sure the spear moves back
+                {
+                    MovementFactor -= 2.4f;
+                }
+                else // Otherwise, increase the movement factor
+                {
+                    MovementFactor += 2.1f;
+                }
+            }
+            // Change the spear position based off of the velocity and the movementFactor
+            projectile.position += projectile.velocity * MovementFactor;
+            // When we reach the end of the animation, we can kill the spear projectile
+            if (projOwner.itemAnimation == 0)
+            {
+                projectile.Kill();
+            }
+            // Apply proper rotation, with an offset of 135 degrees due to the sprite's rotation, notice the usage of MathHelper, use this class!
+            // MathHelper.ToRadians(xx degrees here)
+            projectile.rotation = projectile.velocity.ToRotation() + MathHelper.ToRadians(135f);
+            // Offset by 90 degrees here
+            if (projectile.spriteDirection == -1)
+            {
+                projectile.rotation -= MathHelper.ToRadians(90f);
+            }
+        }
+
+        public override void OnHitNPC(NPC target, int damage, float knockBack, bool crit)
+        {
+            Player player = Main.player[projectile.owner];
+            float screenX = Main.screenPosition.X;
+            if (player.direction < 0)
+            {
+                screenX += Main.screenWidth;
+            }
+            
+            //change to make more/less projectiles
+            for(int i = 0; i < 5; i++)
+            {
+                float screenY = Main.screenPosition.Y;
+                screenY += Main.rand.Next(Main.screenHeight);
+                Vector2 vector = new Vector2(screenX, screenY);
+                float velocityX = target.Center.X - vector.X;
+                float velocityY = target.Center.Y - vector.Y;
+                velocityX += Main.rand.Next(-50, 51) * 0.1f;
+                velocityY += Main.rand.Next(-50, 51) * 0.1f;
+                int num5 = 24;
+                float num6 = (float)Math.Sqrt(velocityX * velocityX + velocityY * velocityY);
+                num6 = num5 / num6;
+                velocityX *= num6;
+                velocityY *= num6;
+                Projectile p = Projectile.NewProjectileDirect(new Vector2(screenX, screenY), new Vector2(velocityX, velocityY), mod.ProjectileType<Moonblow>(), damage, 0f, player.whoAmI);
+                p.tileCollide = false;
+            }
+        }
+    }
+}
