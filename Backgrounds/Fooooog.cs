61a04ac6bea757f8e8a755b903338d41172e44e7
Mep Pep <Mitchell_Pepe@yahoo.com>
2018-12-02 18:15:17 -0500

    blaaaaaa

---
+++ Backgrounds/Fooooog.cs
@@ -0,0 +1,23 @@
+﻿using AAMod.Buffs;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace AAMod.Backgrounds
+{
+    class Fooooog : ModWorld
+    {
+        public override void PostUpdate()
+        {
+            ScreenFog.Update(mod.GetTexture("Backgrounds/fog"));
+        }
+
+        public override void PostDrawTiles()
+        {
+            Player player = Main.player[Main.myPlayer];
+            if (player.HasBuff(mod.BuffType<Clueless>()) && Main.netMode == 0)
+            {
+                ScreenFog.Draw(mod.GetTexture("Backgrounds/fog"), 0.3f, 0.1f);
+            }
+        }
+    }
+}
