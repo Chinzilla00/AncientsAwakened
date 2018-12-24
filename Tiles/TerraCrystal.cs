573e94caaa8fda276a315093fcc8fcdc472640b8
Jon Oakeson <38961891+Alphakip@users.noreply.github.com>
2018-12-23 23:55:54 -0700

    Terra shit

---
+++ Tiles/TerraCrystal.cs
@@ -0,0 +1,46 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AAMod.Tiles
+{
+    public class TerraCrystal : ModTile
+    {
+        public override void SetDefaults()
+        {
+            Main.tileSolid[Type] = true;
+            Main.tileBlockLight[Type] = true;
+            //true for block to emit light
+            soundType = 21;
+            Main.tileLighted[Type] = true;
+            dustType = 107;
+            AddMapEntry(new Color(0, 200, 51));
+        }
+
+        public override bool CanKillTile(int i, int j, ref bool blockDamaged)
+        {
+            return false;
+        }
+
+        public override void PostDraw(int i, int j, SpriteBatch spriteBatch)
+        {
+            Tile tile = Main.tile[i, j];
+            Vector2 zero = new Vector2(Main.offScreenRange, Main.offScreenRange);
+            if (Main.drawToScreen)
+            {
+                zero = Vector2.Zero;
+            }
+            int height = tile.frameY == 36 ? 18 : 16;
+            Main.spriteBatch.Draw(mod.GetTexture("Glowmasks/TerraCrystal_Glow"), new Vector2((i * 16) - (int)Main.screenPosition.X, (j * 16) - (int)Main.screenPosition.Y) + zero, new Rectangle(tile.frameX, tile.frameY, 16, height), AAColor.TerraGlow, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
+        }
+
+        public override void ModifyLight(int i, int j, ref float r, ref float g, ref float b)   //light colors
+        {
+            r = 0;
+            g = 0.10f;
+            b = 0f;
+        }
+    }
+}
