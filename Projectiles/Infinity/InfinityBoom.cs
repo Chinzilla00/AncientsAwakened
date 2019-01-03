7e1c100fe04f7b68e58f3e83fee309c16d3ce874
Jon Oakeson <38961891+Alphakip@users.noreply.github.com>
2019-01-02 23:11:55 -0700

    fix

--- Projectiles/Infinity/InfinityBoom.cs
+++ Projectiles/Infinity/InfinityBoom.cs
@@ -3,7 +3,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 using Terraria.ModLoader;
 
-namespace AAMod.Projectiles.Yamata
+namespace AAMod.Projectiles.Infinity
 {
     public class InfinityBoom : ModProjectile
     {
