eb7a701fe2b585b8822f0a7bf4fe175303a1f1c8
TailstheFox92 <41931137+TailstheFox92@users.noreply.github.com>
2019-01-24 10:32:00 -0500

    Fixed MeteorStrike Projectiles

--- Projectiles/Shen/Meteor.cs
+++ Projectiles/Shen/Meteor.cs
@@ -12,7 +12,7 @@ namespace AAMod.Projectiles.Shen
 
         public override void SetStaticDefaults()
         {
-            Main.projFrames[projectile.type] = 3;
+            Main.projFrames[projectile.type] = 4;
         }
 
         public override Color? GetAlpha(Color lightColor)
