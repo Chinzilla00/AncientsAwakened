59d446a911e833ff372767de0dbfcede7a3b3d2b
Jon Oakeson <38961891+Alphakip@users.noreply.github.com>
2019-01-26 12:17:24 -0700

    hydra fix

--- NPCs/Bosses/Hydra/HydraHead3.cs
+++ NPCs/Bosses/Hydra/HydraHead3.cs
@@ -6,12 +6,14 @@ using Microsoft.Xna.Framework.Graphics;
 namespace AAMod.NPCs.Bosses.Hydra
 {
     [AutoloadBossHead]
-    public class HydraHead3 : HydraHead2
+    public class HydraHead3 : HydraHead1
     {
         public override void SetDefaults()
         {
             base.SetDefaults();
             leftHead = true;
+			middleHead = false;		
+			distFromBodyY = 70;			
         }
     }
 }
