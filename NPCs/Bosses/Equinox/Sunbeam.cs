661e63f7cbf5b49ce8622dbb3351294742a87c9d
Jon Oakeson <38961891+Alphakip@users.noreply.github.com>
2019-01-26 16:11:57 -0700

    ok

---
+++ NPCs/Bosses/Equinox/Sunbeam.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace AAMod.NPCs.Bosses.Equinox
+{
+    public class Sunbeam : ModProjectile
+    {
+    	public int splitTimer = 100;
+    	
+    	public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Sunbeam");
+		}
+    	
+        public override void SetDefaults()
+        {
+            projectile.width = 10;
+            projectile.height = 10;
+            projectile.hostile = true;
+            projectile.scale = 2f;
+            projectile.ignoreWater = true;
+            projectile.penetrate = -1;
+            projectile.alpha = 60;
+            cooldownSlot = 1;
+        }
+
+        public override void AI()
+        {
+        	splitTimer--;
+        	if (splitTimer <= 0)
+        	{
+	        	int numProj = 3;
+	            float rotation = MathHelper.ToRadians(20);
+	            if (projectile.owner == Main.myPlayer)
+	            {
+		            for (int i = 0; i < numProj + 1; i++)
+		            {
+		                Vector2 perturbedSpeed = new Vector2(projectile.velocity.X, projectile.velocity.Y).RotatedBy(MathHelper.Lerp(-rotation, rotation, i / (numProj - 1)));
+		                Projectile.NewProjectile(projectile.Center.X, projectile.Center.Y, perturbedSpeed.X, perturbedSpeed.Y, mod.ProjectileType("SunbeamScatter"), (int)projectile.damage, projectile.knockBack, projectile.owner, 0f, 0f);
+		            }
+	            }
+	            projectile.Kill();
+        	}
+        	Lighting.AddLight(projectile.Center, ((255 - projectile.alpha) * 0.5f) / 255f, ((255 - projectile.alpha) * 0.5f) / 255f, ((255 - projectile.alpha) * 0.05f) / 255f);
+        	projectile.velocity.X *= 1.05f;
+        	projectile.velocity.Y *= 1.05f;
+        	projectile.rotation = (float)Math.Atan2(projectile.velocity.Y, projectile.velocity.X) + 1.57f;
+        }
+    }
+}
