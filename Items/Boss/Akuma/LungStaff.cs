a53c3841867bca21d9352f95df33c566ed7eb033
TailstheFox92 <41931137+TailstheFox92@users.noreply.github.com>
2019-01-29 18:55:36 -0500

    Updated glowmask to LungStaff

--- Items/Boss/Akuma/LungStaff.cs
+++ Items/Boss/Akuma/LungStaff.cs
@@ -8,7 +8,7 @@ using Terraria.ModLoader;
 
 namespace AAMod.Items.Boss.Akuma
 {
-    public class LungStaff : ModItem
+    public class LungStaff : BaseAAItem
     {
         public override void SetStaticDefaults()
         {
@@ -34,28 +34,10 @@ namespace AAMod.Items.Boss.Akuma
             item.buffType = mod.BuffType("LungMinion");
             item.buffTime = 3600;
             item.summon = true;
-        }
 
-
-        public override void PostDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, float rotation, float scale, int whoAmI)
-        {
-            Texture2D texture = mod.GetTexture("Glowmasks/" + GetType().Name + "_Glow");
-            spriteBatch.Draw
-            (
-                texture,
-                new Vector2
-                (
-                    item.position.X - Main.screenPosition.X + item.width * 0.5f,
-                    item.position.Y - Main.screenPosition.Y + item.height - texture.Height * 0.5f + 2f
-                ),
-                new Rectangle(0, 0, texture.Width, texture.Height),
-                Color.White,
-                rotation,
-                texture.Size() * 0.5f,
-                scale,
-                SpriteEffects.None,
-                0f
-            );
+            glowmaskTexture = "Glowmasks/" + GetType().Name + "_Glow"; //the glowmask texture path.
+            glowmaskDrawType = BaseAAItem.GLOWMASKTYPE_SWORD; //what type it is when drawn in the hand, _NONE == no draw, _SWORD == like a sword, _GUN == like a gun	
+            glowmaskDrawColor = Color.White;  //glowmask draw color
         }
 
         public override void ModifyTooltips(List<TooltipLine> list)
