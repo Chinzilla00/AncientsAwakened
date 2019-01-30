995a4c7af67fa7fde7086e019b6be0d59452a0c6
Jon Oakeson <38961891+Alphakip@users.noreply.github.com>
2019-01-29 18:09:10 -0700

    Deity Eater

--- NPCs/Bosses/SoC/Bosses/DeityEater.cs
+++ NPCs/Bosses/SoC/Bosses/DeityEater.cs
@@ -53,28 +53,6 @@ namespace AAMod.NPCs.Bosses.SoC.Bosses
 
         public override void AI()
         {
-            if (Main.netMode != 1)
-            {
-                if (npc.ai[0] == 0)
-                {
-                    npc.realLife = npc.whoAmI;
-                    int latestNPC = npc.whoAmI;
-                    int Length = 30;
-                    for (int i = 0; i < Length; ++i)
-                    {
-                        latestNPC = NPC.NewNPC((int)npc.Center.X, (int)npc.Center.Y, mod.NPCType("DeityEaterBody"), npc.whoAmI, 0, latestNPC);
-                        Main.npc[(int)latestNPC].realLife = npc.whoAmI;
-                        Main.npc[(int)latestNPC].ai[3] = npc.whoAmI;
-                    }
-
-                    latestNPC = NPC.NewNPC((int)npc.Center.X, (int)npc.Center.Y, mod.NPCType("DeityEaterTail"), npc.whoAmI, 0, latestNPC);
-                    Main.npc[(int)latestNPC].realLife = npc.whoAmI;
-                    Main.npc[(int)latestNPC].ai[3] = npc.whoAmI;
-
-                    npc.ai[0] = 1;
-                    npc.netUpdate = true;
-                }
-            }
             if (npc.alpha != 0)
             {
                 for (int spawnDust = 0; spawnDust < 2; spawnDust++)
@@ -89,116 +67,18 @@ namespace AAMod.NPCs.Bosses.SoC.Bosses
             {
                 npc.alpha = 0;
             }
-            npc.ai[1] = (float)Main.rand.Next(-2, 0);
-            npc.netUpdate = true;
-            int minTilePosX = (int)(npc.position.X / 16.0) - 1;
-            int maxTilePosX = (int)((npc.position.X + npc.width) / 16.0) + 2;
-            int minTilePosY = (int)(npc.position.Y / 16.0) - 1;
-            int maxTilePosY = (int)((npc.position.Y + npc.height) / 16.0) + 2;
-            if (minTilePosX < 0)
-                minTilePosX = 0;
-            if (maxTilePosX > Main.maxTilesX)
-                maxTilePosX = Main.maxTilesX;
-            if (minTilePosY < 0)
-                minTilePosY = 0;
-            if (maxTilePosY > Main.maxTilesY)
-                maxTilePosY = Main.maxTilesY;
-            
 
-            for (int i = minTilePosX; i < maxTilePosX; ++i)
+            BaseAI.AIWorm(npc, new int[] { mod.NPCType("DeityEater"), mod.NPCType("DeityEaterBody"), mod.NPCType("DeityEaterTail") }, 30, 4f, 17f, 0f, true, false, true, false, false, false);
+            bool isBody = npc.type == mod.NPCType("DeityEaterBody");
+            if (isBody)
             {
-                for (int j = minTilePosY; j < maxTilePosY; ++j)
+                if (npc.localAI[0] == 0)
                 {
-                    if (Main.tile[i, j] != null && (Main.tile[i, j].nactive() && (Main.tileSolid[(int)Main.tile[i, j].type] || Main.tileSolidTop[(int)Main.tile[i, j].type] && (int)Main.tile[i, j].frameY == 0) || (int)Main.tile[i, j].liquid > 64))
-                    {
-                        Vector2 vector2;
-                        vector2.X = (float)(i * 16);
-                        vector2.Y = (float)(j * 16);
-                        if (npc.position.X + npc.width > vector2.X && npc.position.X < vector2.X + 16.0 && (npc.position.Y + npc.height > (double)vector2.Y && npc.position.Y < vector2.Y + 16.0))
-                        {
-                            if (Main.rand.Next(100) == 0 && Main.tile[i, j].nactive())
-                                WorldGen.KillTile(i, j, true, true, false);
-                        }
-                    }
+                    npc.localAI[0] = 1;
+                    npc.localAI[1] = Main.rand.Next(4);
                 }
+                npc.frame.Y = (int)npc.localAI[1] * npc.frame.Height;
             }
-            float speed = 11f;
-            float acceleration = 0.18f;
-
-            Vector2 npcCenter = new Vector2(npc.position.X + npc.width * 0.5f, npc.position.Y + npc.height * 0.5f);
-            float targetXPos = Main.player[npc.target].position.X + (Main.player[npc.target].width / 2);
-            float targetYPos = Main.player[npc.target].position.Y + (Main.player[npc.target].height / 2);
-
-            float targetRoundedPosX = (float)((int)(targetXPos / 16.0) * 16);
-            float targetRoundedPosY = (float)((int)(targetYPos / 16.0) * 16);
-            npcCenter.X = (float)((int)(npcCenter.X / 16.0) * 16);
-            npcCenter.Y = (float)((int)(npcCenter.Y / 16.0) * 16);
-            float dirX = targetRoundedPosX - npcCenter.X;
-            float dirY = targetRoundedPosY - npcCenter.Y;
-            npc.TargetClosest(true);
-            float length = (float)Math.Sqrt(dirX * dirX + dirY * dirY);
-
-            float absDirX = Math.Abs(dirX);
-            float absDirY = Math.Abs(dirY);
-            float newSpeed = speed / length;
-            dirX = dirX * (newSpeed * 2);
-            dirY = dirY * (newSpeed * 2);
-            if (npc.velocity.X > 0.0 && dirX > 0.0 || npc.velocity.X < 0.0 && dirX < 0.0 || (npc.velocity.Y > 0.0 && dirY > 0.0 || npc.velocity.Y < 0.0 && dirY < 0.0))
-            {
-                if (npc.velocity.X < dirX)
-                    npc.velocity.X = npc.velocity.X + acceleration;
-                else if (npc.velocity.X > dirX)
-                    npc.velocity.X = npc.velocity.X - acceleration;
-                if (npc.velocity.Y < dirY)
-                    npc.velocity.Y = npc.velocity.Y + acceleration;
-                else if (npc.velocity.Y > dirY)
-                    npc.velocity.Y = npc.velocity.Y - acceleration;
-                if (Math.Abs(dirY) < speed * 0.2 && (npc.velocity.X > 0.0 && dirX < 0.0 || npc.velocity.X < 0.0 && dirX > 0.0))
-                {
-                    if (npc.velocity.Y > 0.0)
-                        npc.velocity.Y = npc.velocity.Y + acceleration * 2f;
-                    else
-                        npc.velocity.Y = npc.velocity.Y - acceleration * 2f;
-                }
-                if (Math.Abs(dirX) < speed * 0.2 && (npc.velocity.Y > 0.0 && dirY < 0.0 || npc.velocity.Y < 0.0 && dirY > 0.0))
-                {
-                    if (npc.velocity.X > 0.0)
-                        npc.velocity.X = npc.velocity.X + acceleration * 2f;
-                    else
-                        npc.velocity.X = npc.velocity.X - acceleration * 2f;
-                }
-            }
-            else if (absDirX > absDirY)
-            {
-                if (npc.velocity.X < dirX)
-                    npc.velocity.X = npc.velocity.X + acceleration * 1.1f;
-                else if (npc.velocity.X > dirX)
-                    npc.velocity.X = npc.velocity.X - acceleration * 1.1f;
-
-                if (Math.Abs(npc.velocity.X) + Math.Abs(npc.velocity.Y) < speed * 0.5)
-                {
-                    if (npc.velocity.Y > 0.0)
-                        npc.velocity.Y = npc.velocity.Y + acceleration;
-                    else
-                        npc.velocity.Y = npc.velocity.Y - acceleration;
-                }
-            }
-            else
-            {
-                if (npc.velocity.Y < dirY)
-                    npc.velocity.Y = npc.velocity.Y + acceleration * 1.1f;
-                else if (npc.velocity.Y > dirY)
-                    npc.velocity.Y = npc.velocity.Y - acceleration * 1.1f;
-
-                if (Math.Abs(npc.velocity.X) + Math.Abs(npc.velocity.Y) < speed * 0.5)
-                {
-                    if (npc.velocity.X > 0.0)
-                        npc.velocity.X = npc.velocity.X + acceleration;
-                    else
-                        npc.velocity.X = npc.velocity.X - acceleration;
-                }
-            }
-            npc.rotation = (float)Math.Atan2((double)npc.velocity.Y, (double)npc.velocity.X) + 1.57f;
         }
 
         public override bool StrikeNPC(ref double damage, int defense, ref float knockback, int hitDirection, ref bool crit)
