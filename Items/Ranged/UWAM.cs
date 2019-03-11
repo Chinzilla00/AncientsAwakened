ff873b63082f250915efb603435b42e757a144ce
VVV101 <VVVHUNTER@yandex.ru>
2019-03-11 09:26:47 +0600

    Some EX Weapons
    Nova Flare (Lunar Flare EX), U.W.A.M. (S.D.M.G. EX), Blizzard Dragon Staff (Frost Hydra Staff EX).

---
+++ Items/Ranged/UWAM.cs
@@ -0,0 +1,92 @@
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Localization;
+
+namespace AAMod.Items.Ranged
+{
+	public class UWAM : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("U.W.A.M.");
+			Tooltip.SetDefault("Shoots hundreds of bullets with a very low spread"
+			+"\nHave a chance to shoot sharks, dealing 3x damage"
+			+"\n88% chance not to consume ammo"
+			+"\nS.D.M.G. EX");
+        }
+
+		public override void SetDefaults()
+		{
+			item.CloneDefaults(ItemID.SDMG);
+			item.damage = 60;
+			item.ranged = true;
+			item.knockBack = 4;
+			item.width = 86;
+			item.height = 40;
+			item.useTime = 4;
+			item.useAnimation = 4;
+			item.value = 1000000;
+			item.rare = 11;
+			item.autoReuse = true;
+		}
+		
+		public override bool ConsumeAmmo(Player player)
+		{
+			return Main.rand.NextFloat() >= .88;
+		}
+		
+		public override Vector2? HoldoutOffset()
+		{
+			return new Vector2(-16, 0);
+		}
+		
+		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+		{
+			if (Main.rand.NextBool(10))
+			{
+				type = 408;
+				speedX *= 4;
+				speedY *= 4;
+				damage *= 3;
+			}
+			else
+			{
+			}
+			
+			Vector2 vector = player.RotatedRelativePoint(player.MountedCenter, true);
+			Vector2 perturbedSpeed2 = new Vector2(speedX, speedY).RotatedByRandom(MathHelper.ToRadians(2));
+			Vector2 perturbedSpeed3 = new Vector2(speedX, speedY).RotatedByRandom(MathHelper.ToRadians(2));
+			float speedX2 = perturbedSpeed2.X;
+			float speedY2 = perturbedSpeed2.Y;
+			float speedX3 = perturbedSpeed3.X;
+			float speedY3 = perturbedSpeed3.Y;
+			int p1 = Projectile.NewProjectile(vector.X, vector.Y, speedX2, speedY2, type, damage, knockBack, player.whoAmI);
+			int p2 = Projectile.NewProjectile(vector.X, vector.Y, speedX, speedY, type, damage, knockBack, player.whoAmI);
+			int p3 = Projectile.NewProjectile(vector.X, vector.Y, speedX3, speedY3, type, damage, knockBack, player.whoAmI);
+			if (type == 408)
+			{
+				Main.projectile[p1].minion = false;
+				Main.projectile[p1].ranged = true;
+				Main.projectile[p2].minion = false;
+				Main.projectile[p2].ranged = true;
+				Main.projectile[p3].minion = false;
+				Main.projectile[p3].ranged = true;
+			}
+			return false;
+		}
+		
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.SDMG);
+			recipe.AddIngredient(null, "EXSoul");
+            recipe.AddTile(null, "QuantumFusionAccelerator");
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
---
+++ Items/Summoning/BlizzardDragonStaff.cs
@@ -0,0 +1,80 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AAMod.Items.Summoning
+{
+    public class BlizzardDragonStaff : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Blizzard Dragon Staff");
+            Tooltip.SetDefault(@"Summons a blizzard dragon to fight for you");
+        }
+
+        public override void SetDefaults()
+        {
+            item.useStyle = 1;
+            item.shootSpeed = 14f;
+            item.shoot = mod.ProjectileType("BlizzardDragon");
+            item.damage = 222;
+            item.width = 50;
+            item.height = 50;
+            item.UseSound = SoundID.Item44;
+            item.useAnimation = 30;
+            item.useTime = 30;
+            item.noMelee = true;
+            item.value = Item.sellPrice(0, 0, 27, 0);
+            item.knockBack = 8f;
+            item.rare = 11;
+            item.summon = true;
+            item.mana = 50;
+            item.sentry = true;
+        }
+
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            int i = Main.myPlayer;
+            int num74 = item.shoot;
+            int num76 = item.damage;
+            float num77 = item.knockBack;
+            int num154 = (int)((float)Main.mouseX + Main.screenPosition.X) / 16;
+            int num155 = (int)((float)Main.mouseY + Main.screenPosition.Y) / 16;
+            if (player.gravDir == -1f)
+            {
+                num155 = (int)(Main.screenPosition.Y + (float)Main.screenHeight - (float)Main.mouseY) / 16;
+            }
+			if (player.ownedProjectileCounts[num74] < player.maxTurrets)
+			{
+				Projectile.NewProjectile((float)Main.mouseX + Main.screenPosition.X, (float)(num155 * 16 - 24), 0f, 15f, num74, num76, num77, i, 0f, 0f);
+			}
+			if (player.ownedProjectileCounts[num74] == player.maxTurrets)
+			{
+				for(int g = 0; g < 1000; ++g)
+				{
+					if(Main.projectile[g].active && Main.projectile[g].type == num74)
+					{
+						Main.projectile[g].Kill();
+						break;
+					}
+				}
+				 Projectile.NewProjectile((float)Main.mouseX + Main.screenPosition.X, (float)(num155 * 16 - 24), 0f, 15f, num74, num76, num77, i, 0f, 0f);
+			}
+
+            return false;
+        }
+
+        public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(1572);
+			recipe.AddIngredient(null, "EXSoul");
+			recipe.AddTile(null, "QuantumFusionAccelerator");
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+    }
+}
