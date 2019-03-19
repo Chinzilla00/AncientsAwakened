5f94390e7a1489a459e78f869aa3f1a95a3bdc13
TailstheFox92 <41931137+TailstheFox92@users.noreply.github.com>
2019-03-19 13:08:33 -0400

    The Freedom Star is finally complete!

--- Projectiles/FreedomShotCharged.cs
+++ Projectiles/FreedomShotCharged.cs
@@ -10,7 +10,7 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace AAMod.Projectiles
 {
-    public class FreedomShot : ModProjectile
+    public class FreedomShotCharged : ModProjectile
     {
         public override void SetStaticDefaults()
         {
@@ -21,7 +21,7 @@ namespace AAMod.Projectiles
         public override void SetDefaults()
         {
             projectile.width = 36;
-            projectile.height = 28;
+            projectile.height = 32;
             projectile.friendly = true;
             projectile.ranged = true;
             projectile.ignoreWater = true;
@@ -50,7 +50,10 @@ namespace AAMod.Projectiles
 		public override void Kill(int timeLeft)
 		{
 			Main.PlaySound(SoundID.DD2_ExplosiveTrapExplode, projectile.position);
-			for (int index1 = 0; index1 < 20; ++index1)
+			int p = Projectile.NewProjectile(projectile.Center.X, projectile.Center.Y, 0f, 0f, mod.ProjectileType("DummyExplosionTerra"), projectile.damage, 0, Main.myPlayer);
+			Main.projectile[p].magic = false;
+			Main.projectile[p].ranged = true;
+			for (int index1 = 0; index1 < 30; ++index1)
 			{
 				int index2 = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, 74, 0.0f, 0.0f, 100, new Color(), 1f);
 				Main.dust[index2].velocity *= 1.1f;
