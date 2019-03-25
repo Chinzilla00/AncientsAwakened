4ad816c05e168579f0283fd9461e8c0fe522c030
Jon Oakeson <38961891+Alphakip@users.noreply.github.com>
2019-03-25 08:53:43 -0600

    0.6.3.0

--- Projectiles/AH/Surasshu.cs
+++ Projectiles/AH/Surasshu.cs
@@ -4,7 +4,7 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace AAMod.Projectiles.Sisters
+namespace AAMod.Projectiles.AH
 {
     public class Surasshu : ModProjectile
     {
