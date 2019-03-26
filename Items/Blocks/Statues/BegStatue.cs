f21c0bcddafcf88656eec401446b9bf3c1d481d2
Jon Oakeson <38961891+Alphakip@users.noreply.github.com>
2019-03-26 09:16:40 -0600

    Statues

---
+++ Items/Blocks/Statues/BegStatue.cs
@@ -0,0 +1,37 @@
+using Terraria.ModLoader; using Terraria.ID;
+
+namespace AAMod.Items.Blocks.Statues
+{
+	public class BegStatue : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Weird Horse Statue");
+        }
+
+        public override void SetDefaults()
+		{
+			item.width = 30;
+			item.height = 30;
+			item.maxStack = 99;
+			item.useTurn = true;
+			item.autoReuse = true;
+			item.useAnimation = 15;
+			item.useTime = 10;
+			item.useStyle = 1;
+			item.consumable = true;
+			item.value = 50000;
+			item.rare = 1;
+			item.createTile = mod.TileType("DevStatue");
+			item.placeStyle = 10;
+        }
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.StoneBlock, 50);
+            recipe.AddTile(TileID.HeavyWorkBench);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
---
+++ Items/Blocks/Statues/KyuuStatue.cs
@@ -0,0 +1,38 @@
+using Terraria.ModLoader; using Terraria.ID;
+
+namespace AAMod.Items.Blocks.Statues
+{
+	public class KyuuStatue : ModItem
+	{
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Excaligrim Statue");
+        }
+
+        public override void SetDefaults()
+		{
+			item.width = 30;
+			item.height = 30;
+			item.maxStack = 99;
+			item.useTurn = true;
+			item.autoReuse = true;
+			item.useAnimation = 15;
+			item.useTime = 10;
+			item.useStyle = 1;
+			item.consumable = true;
+			item.value = 50000;
+			item.rare = 1;
+			item.createTile = mod.TileType("DevStatue");
+			item.placeStyle = 9;
+        }
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.StoneBlock, 50);
+            recipe.AddTile(TileID.HeavyWorkBench);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
