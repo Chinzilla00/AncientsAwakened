71185b5ac7567fdfec2513eb4039f980f2bcec6f
Tails The Fox <Tails_the_Fox87@outlook.com>
2019-04-21 13:38:09 -0400

    Fixed an error in ShenBreath and Vortex visual glitch (Item was trying to use a glowmask that doesn't belong to it because it doesn't have a glowmask.)

--- NPCs/Bosses/Shen/ShenBreath.cs
+++ NPCs/Bosses/Shen/ShenBreath.cs
@@ -47,7 +47,7 @@ namespace AAMod.NPCs.Bosses.Shen
                     num296 = 0.75f;
                 }
                 projectile.ai[0] += 1f;
-                int num297 = Main.rand.Next(2) ? mod.DustType<Dusts.AkumaADust>() : mod.DustType<Dusts.YamataADust>();
+                int num297 = Main.rand.Next(2) == 0 ? mod.DustType<Dusts.AkumaADust>() : mod.DustType<Dusts.YamataADust>();
                 if (Main.rand.Next(2) == 0)
                 {
                     for (int num298 = 0; num298 < 4; num298++)
