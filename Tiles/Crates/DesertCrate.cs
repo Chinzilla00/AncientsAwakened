fd2dd61afeb06f27ead6ec778fac5d7522290ffa
Hallam9K <32880148+Hallam9K@users.noreply.github.com>
2019-04-26 18:22:39 +0100

    Update DesertCrate.cs

--- Tiles/Crates/DesertCrate.cs
+++ Tiles/Crates/DesertCrate.cs
@@ -13,6 +13,7 @@ namespace AAMod.Tiles.Crates
             ModTranslation name = CreateMapEntryName();
             name.SetDefault("Desert Crate");
             Main.tileFrameImportant[Type] = true;
+            Main.tileTable[Type] = true;
             Main.tileSolidTop[Type] = true;
             TileObjectData.newTile.CopyFrom(TileObjectData.Style2x2);
             TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16 };
