9f23f47670f1c6aa22c261692ed21a8e36c2d1b0
Jon Oakeson <38961891+Alphakip@users.noreply.github.com>
2019-05-09 11:33:07 -0600

    Volley Tweaks

--- Items/Magic/Volley.cs
+++ Items/Magic/Volley.cs
@@ -24,7 +24,7 @@ namespace AAMod.Items.Magic
             item.UseSound = SoundID.Item21;
             item.autoReuse = true;
             item.shoot = mod.ProjectileType("Volley");
-            item.shootSpeed = 7f;
+            item.shootSpeed = 5f;
         }
 
         public override void SetStaticDefaults()
@@ -34,23 +34,17 @@ namespace AAMod.Items.Magic
 
         public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
         {
-            Vector2 vector2 = player.RotatedRelativePoint(player.MountedCenter, true);
-            float num81 = Main.mouseX + Main.screenPosition.X - vector2.X;
-            float num82 = Main.mouseY + Main.screenPosition.Y - vector2.Y;
-            Vector2 value12 = Vector2.Normalize(new Vector2(num81, num82)) * 40f * item.scale;
-            if (Collision.CanHit(vector2, 0, 0, vector2 + value12, 0, 0))
+            float spread = 45f * 0.0174f;
+            float baseSpeed = (float)Math.Sqrt((speedX * speedX) + (speedY * speedY));
+            double startAngle = Math.Atan2(speedX, speedY) - .1d;
+            double deltaAngle = spread / 6f;
+            double offsetAngle;
+            for (int i = 0; i < 3; i++)
             {
-                vector2 += value12;
-            }
-            Vector2 vector16 = new Vector2(num81, num82);
-            vector16 *= 0.8f;
-            Vector2 value13 = vector16.SafeNormalize(-Vector2.UnitY);
-            float num202 = 0.0174532924f * (float)(-(float)player.direction);
-            for (int num203 = 0; num203 <= 1; num203++)
-            {
-                Projectile.NewProjectile(vector2, (vector16 + value13 * (float)num203 * 1f).RotatedBy((double)((float)num203 * num202), default(Vector2)), type, damage, knockBack, item.owner, 0f, 0f);
+                offsetAngle = startAngle + (deltaAngle * i);
+                Projectile.NewProjectile(position.X, position.Y, baseSpeed * (float)Math.Sin(offsetAngle), baseSpeed * (float)Math.Cos(offsetAngle), item.shoot, damage, knockBack, item.owner);
             }
             return false;
         }
-	}
+    }
 }
