d766dc62356c65f76079819fc4f512ecdb1e758b
TheDoctorDallin <44709528+TheDoctorDallin@users.noreply.github.com>
2019-06-05 14:25:08 -0600

    Added Chronos and Time Teller fixes

--- Items/Dev/Chronos.cs
+++ Items/Dev/Chronos.cs
@@ -1,6 +1,7 @@
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using BaseMod;
 
 namespace AAMod.Items.Dev
 {
@@ -23,18 +24,24 @@ namespace AAMod.Items.Dev
             item.expert = true;
         }
 
+		public override void GetWeaponDamage(Player player, ref int damage)
+		{
+			damage = (int)((float)damage * CalcDamageMultiplierFromTimeOfDay(item.damage));
+		}
 
-        public override void PostUpdate()
+        public override void OnHitNPC(Player player, NPC target, int damage, float knockBack, bool crit)
         {
-            item.damage = Damage() + 50;
+            target.AddBuff(BuffID.Chilled, 1000);
         }
 
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Chronos");
-            Tooltip.SetDefault("Time Teller EX \n" +
-                "Time is big ball of wibbly-wobbly timey-wimey yo-yos. \n" +
-                "-Dallin");
+            Tooltip.SetDefault("Time Teller EX\n" +
+				"Damage changes based on time of day\n" +
+				"Damage is greatest at Midday and Midnight\n" +			
+                "'Time is big ball of wibbly-wobbly timey-wimey yo-yos.'\n" +
+                "-Dallin");			
         }
 
         public override void UpdateInventory(Player player)
@@ -52,116 +59,20 @@ namespace AAMod.Items.Dev
 			recipe.AddRecipe();
         }
 
-        public int Damage()
-        {
-            double num4 = (float)Main.time;
-            if (!Main.dayTime)
-            {
-                num4 += 54000.0;
-            }
-            num4 = num4 / 86400.0 * 24.0;
-            double num5 = 7.5;
-            num4 = num4 - num5 - 12.0;
-            if (num4 < 0.0)
-            {
-                num4 += 24.0;
-            }
-            if (num4 == 1)
-            {
-                return 30;
-            }
-            else if (num4 == 2)
-            {
-                return 60;
-            }
-            else if (num4 == 3)
-            {
-                return 90;
-            }
-            else if (num4 == 4)
-            {
-                return 120;
-            }
-            else if (num4 == 5)
-            {
-                return 150;
-            }
-            else if (num4 == 6)
-            {
-                return 180;
-            }
-            else if (num4 == 7)
-            {
-                return 210;
-            }
-            else if (num4 == 8)
-            {
-                return 240;
-            }
-            else if (num4 == 9)
-            {
-                return 270;
-            }
-            else if (num4 == 10)
-            {
-                return 300;
-            }
-            else if (num4 == 11)
-            {
-                return 330;
-            }
-            else if (num4 == 12)
-            {
-                return 360;
-            }
-            else if (num4 == 13)
-            {
-                return 30;
-            }
-            else if (num4 == 14)
-            {
-                return 60;
-            }
-            else if (num4 == 15)
-            {
-                return 90;
-            }
-            else if (num4 == 16)
-            {
-                return 120;
-            }
-            else if (num4 == 17)
-            {
-                return 150;
-            }
-            else if (num4 == 18)
-            {
-                return 180;
-            }
-            else if (num4 == 19)
-            {
-                return 210;
-            }
-            else if (num4 == 20)
-            {
-                return 240;
-            }
-            else if (num4 == 21)
-            {
-                return 270;
-            }
-            else if (num4 == 22)
-            {
-                return 300;
-            }
-            else if (num4 == 23)
-            {
-                return 330;
-            }
-            else
-            {
-                return 360;
-            }
-        }
+		public float CalcDamageMultiplierFromTimeOfDay(int baseDamage)
+		{
+			int minDamage = baseDamage; //this is the damage you set in SetDefaults.
+			int maxDamage = 500; //this is the damage you get at midday/midnight.
+
+			float maxMultiplier = ((float)maxDamage / (float)minDamage);		
+			float time = (int)Main.time;
+			float calcTimeMax = 0f;
+			if(Main.dayTime)
+				calcTimeMax = 54000f; //max time in a day
+			else
+				calcTimeMax = 32400f; //max time in a night
+
+			return BaseUtility.MultiLerp(time / calcTimeMax, 1f, maxMultiplier, 1f);
+		}
     }
 }
