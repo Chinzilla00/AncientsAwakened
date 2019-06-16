b280e580a212f73a01ae4981a2e4864e046bbbf7
Alphakip <38961891+Alphakip@users.noreply.github.com>
2019-06-16 13:37:11 -0600

    Darkpuppey Projectiles

--- Projectiles/Darkpuppey/A.cs
+++ Projectiles/Darkpuppey/A.cs
@@ -6,21 +6,23 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace AAMod.Projectiles
+namespace AAMod.Projectiles.Darkpuppey
 {
-    public class ChaosYariEXShot : ModProjectile
+    public class A : ModProjectile
     {
         public bool spineEnd = false;
+        public bool spineBeginning = false;
 
         public override void SetStaticDefaults()
         {
-            DisplayName.SetDefault("Perfect Chaos Yari");
+            DisplayName.SetDefault("Decieving Truth");
+            Main.projFrames[projectile.type] = 3;
         }
 
         public override void SetDefaults()
         {
-            projectile.width = 28;
-            projectile.height = 26;
+            projectile.width = 34;
+            projectile.height = 32;
             projectile.aiStyle = -1;
             projectile.timeLeft = 320;
             projectile.friendly = true;
@@ -30,15 +32,28 @@ namespace AAMod.Projectiles
             projectile.damage = 1;
             projectile.penetrate = -1;
             projectile.alpha = 255;
-            projectile.melee = true;
+            projectile.magic = true;
             projectile.usesLocalNPCImmunity = true;
-            projectile.localNPCHitCooldown = 12;
+            projectile.localNPCHitCooldown = 20;
         }
 
         public override void AI()
         {
             BaseMod.BaseAI.AIVilethorn(projectile, 70, 4, 10);
+            spineBeginning = projectile.ai[1] == 0;
             spineEnd = projectile.ai[1] == 10;
+            if (projectile.ai[1] == 0)
+            {
+                projectile.frame = 2;
+            }
+            else if (projectile.ai[1] == 10)
+            {
+                projectile.frame = 0;
+            }
+            else
+            {
+                projectile.frame = 1;
+            }
         }
 
         public override void PostAI()
@@ -47,22 +62,17 @@ namespace AAMod.Projectiles
             {
                 for (int j = 0; j < 4; j++)
                 {
-                    int DustType = mod.DustType<Dusts.Discord>();
-                    Dust.NewDust(projectile.position, projectile.width, projectile.height, 48, projectile.velocity.X * 0.025f, projectile.velocity.Y * 0.025f, DustType, Color.White, j == 0 ? 1.1f : 1.2f);
+                    Dust.NewDust(projectile.position, projectile.width, projectile.height, 48, projectile.velocity.X * 0.025f, projectile.velocity.Y * 0.025f, 6, Color.White, j == 0 ? 1.1f : 1.2f);
                 }
             }
         }
 
-        private Texture2D mainTex;
-        private Texture2D endTex;
-
         public override bool PreDraw(SpriteBatch sb, Color drawColor)
         {
-            Color lightColor = new Color(0, 0, 200);
-            Color newLightColor = new Color(Math.Max(0, lightColor.R + Math.Min(0, -projectile.alpha + 20)), Math.Max(0, lightColor.G + Math.Min(0, -projectile.alpha + 20)), Math.Max(0, lightColor.B + Math.Min(0, -projectile.alpha + 20)));
+            Color newLightColor = new Color(Math.Max(0, Color.Orange.R + Math.Min(0, -projectile.alpha + 20)), Math.Max(0, Color.Orange.G + Math.Min(0, -projectile.alpha + 20)), Math.Max(0, Color.Orange.B + Math.Min(0, -projectile.alpha + 20)));
             BaseMod.BaseDrawing.AddLight(projectile.Center, newLightColor);
-            if (mainTex == null) { mainTex = Main.projectileTexture[projectile.type]; endTex = mod.GetTexture("Projectiles/ChaosYariEXShot_Tip"); }
-            BaseMod.BaseDrawing.DrawTexture(sb, spineEnd ? endTex : mainTex, 0, projectile);
+            Texture2D mainTex = Main.projectileTexture[projectile.type];
+            BaseMod.BaseDrawing.DrawTexture(sb, mainTex, 0, projectile);
             return false;
         }
     }
---
+++ Projectiles/Darkpuppey/F.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AAMod.Projectiles.Darkpuppey
+{
+    public class F : ModProjectile
+    {
+        public bool spineEnd = false;
+        public bool spineBeginning = false;
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Decieving Truth");
+            Main.projFrames[projectile.type] = 3;
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.width = 34;
+            projectile.height = 32;
+            projectile.aiStyle = -1;
+            projectile.timeLeft = 320;
+            projectile.friendly = true;
+            projectile.hostile = false;
+            projectile.tileCollide = false;
+            projectile.ignoreWater = true;
+            projectile.damage = 1;
+            projectile.penetrate = -1;
+            projectile.alpha = 255;
+            projectile.magic = true;
+            projectile.usesLocalNPCImmunity = true;
+            projectile.localNPCHitCooldown = 20;
+        }
+
+        public override void AI()
+        {
+            BaseMod.BaseAI.AIVilethorn(projectile, 70, 4, 10);
+            spineBeginning = projectile.ai[1] == 0;
+            spineEnd = projectile.ai[1] == 10;
+            if (projectile.ai[1] == 0)
+            {
+                projectile.frame = 2;
+            }
+            else if (projectile.ai[1] == 10)
+            {
+                projectile.frame = 0;
+            }
+            else
+            {
+                projectile.frame = 1;
+            }
+        }
+
+        public override bool PreDraw(SpriteBatch sb, Color drawColor)
+        {
+            Color newLightColor = new Color(Math.Max(0, Color.White.R + Math.Min(0, -projectile.alpha + 20)), Math.Max(0, Color.White.G + Math.Min(0, -projectile.alpha + 20)), Math.Max(0, Color.White.B + Math.Min(0, -projectile.alpha + 20)));
+            BaseMod.BaseDrawing.AddLight(projectile.Center, newLightColor);
+            Texture2D mainTex = Main.projectileTexture[projectile.type];
+            BaseMod.BaseDrawing.DrawTexture(sb, mainTex, 0, projectile);
+            return false;
+        }
+    }
+}
