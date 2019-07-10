146e730bca28b84e14638e6b0131dc635af91648
Alphakip <38961891+Alphakip@users.noreply.github.com>
2019-07-10 10:40:22 -0600

    Update Gigataser.cs

--- Items/Boss/Zero/Gigataser.cs
+++ Items/Boss/Zero/Gigataser.cs
@@ -39,36 +39,12 @@ the longer the taser is charged, the more it penetrates");
 
         public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
         {
-            int[] array4 = new int[5];
-            Vector2[] array5 = new Vector2[5];
-            int num838 = 0;
-            float num839 = 2000f;
-            for (int num840 = 0; num840 < Main.maxNPCs; num840++)
+            for (int num842 = 0; num842 < 3; num842++)
             {
-                if (Main.npc[num840].active)
-                {
-                    Vector2 center9 = Main.npc[num840].Center;
-                    float num841 = Vector2.Distance(center9, position);
-                    if (num841 < num839 && Collision.CanHit(position, 1, 1, center9, 1, 1))
-                    {
-                        array4[num838] = num840;
-                        array5[num838] = center9;
-                        if (++num838 >= array5.Length)
-                        {
-                            break;
-                        }
-                    }
-                }
-            }
-            for (int num842 = 0; num842 < num838; num842++)
-            {
-                Vector2 vector82 = array5[num842] - position;
+                Vector2 vector82 = new Vector2(speedX, speedY);
                 float ai = Main.rand.Next(100);
-                for (int i = 0; i < 3; i++)
-                {
-                    Vector2 vector83 = Vector2.Normalize(vector82.RotatedByRandom(0.78539818525314331)) * 14f;
-                    Projectile.NewProjectile(position.X, position.Y, vector83.X * 2, vector83.Y * 2, mod.ProjectileType<Projectiles.Zero.ZeroTaze>(), damage, 0f, Main.myPlayer, vector82.ToRotation(), ai);
-                }
+                Vector2 vector83 = Vector2.Normalize(vector82.RotatedByRandom(0.78539818525314331)) * 14f;
+                Projectile.NewProjectile(position.X, position.Y, vector83.X * 2, vector83.Y * 2, mod.ProjectileType<Projectiles.Zero.ZeroTaze>(), damage, 0f, Main.myPlayer, vector82.ToRotation(), ai);
             }
             return false;
         }
