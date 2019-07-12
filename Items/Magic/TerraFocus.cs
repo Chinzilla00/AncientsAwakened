dfadad4407aef8eedc2494c56df26abc41fc3c9f
Alphakip <38961891+Alphakip@users.noreply.github.com>
2019-07-12 10:42:24 -0600

    Armor sprites

--- Items/Magic/TerraFocus.cs
+++ Items/Magic/TerraFocus.cs
@@ -26,34 +26,13 @@ namespace AAMod.Items.Magic
             item.damage = 50;
             item.UseSound = SoundID.Item9;
             item.crit = 20;
-            item.shoot = 660;
+            item.shoot = mod.ProjectileType<Projectiles.MagicBlastF>();
             item.mana = 14;
             item.rare = 4;
             item.value = 300000;
             item.noMelee = true;
             item.magic = true;
             item.autoReuse = true;
-        }
-
-        public override void PostDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, float rotation, float scale, int whoAmI)
-        {
-            Texture2D texture = mod.GetTexture("Glowmasks/" + GetType().Name);
-            spriteBatch.Draw
-            (
-                texture,
-                new Vector2
-                (
-                    item.position.X - Main.screenPosition.X + item.width * 0.5f,
-                    item.position.Y - Main.screenPosition.Y + item.height - texture.Height * 0.5f + 2f
-                ),
-                new Rectangle(0, 0, texture.Width, texture.Height),
-                Color.White,
-                rotation,
-                texture.Size() * 0.5f,
-                scale,
-                SpriteEffects.None,
-                0f
-            );
         }
     }
 }
