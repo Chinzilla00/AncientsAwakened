268e3e25769e986f66ca04b5598393f7b968d84e
GovernorGibs <52765683+GovernorGibs@users.noreply.github.com>
2019-07-22 08:47:07 -0700

    Revert "Revert "ssssssssssssssssssssssssss""
    This reverts commit 3e1cd412bb5ea5b1579213e1fc2bea1b6d2b665e.

--- NPCs/Bosses/Yamata/Awakened/YamataAHeadF1.cs
+++ NPCs/Bosses/Yamata/Awakened/YamataAHeadF1.cs
@@ -19,7 +19,7 @@ namespace AAMod.NPCs.Bosses.Yamata.Awakened
         {
             base.SetDefaults();
             npc.lifeMax = 35000;
-            npc.damage = 68;
+            npc.damage = 260;
             npc.width = 46;
             npc.height = 46;
 			isAwakened = true;
