ad67f10fc2cfb829105b25112baf001894a7e758
GovernorGibs <52765683+GovernorGibs@users.noreply.github.com>
2019-07-25 14:05:09 -0700

    Omega Volley 2.0

--- Items/Boss/Zero/OmegaVolley.cs
+++ Items/Boss/Zero/OmegaVolley.cs
@@ -12,15 +12,16 @@ namespace AAMod.Items.Boss.Zero
 		{
 			item.useStyle = 5;
 			item.autoReuse = true;
-			item.useAnimation = 2;
-			item.useTime = 2;
+			item.useAnimation = 22;
+			item.useTime = 4;
+            item.reuseDelay = 22;
 			item.width = 72;
 			item.height = 34;
 			item.shoot = 10;
 			item.useAmmo = AmmoID.Bullet;
 			item.UseSound = SoundID.Item41;
-			item.damage = 100;
-			item.shootSpeed = 15f;
+			item.damage = 50;
+			item.shootSpeed = 5f;
 			item.noMelee = true;
 			item.value = Item.sellPrice(0, 30, 0, 0);
 			item.rare = 11;
@@ -44,12 +45,34 @@ namespace AAMod.Items.Boss.Zero
 		{
 			return new Vector2(-10, -2);
 		}
-		
-		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
-		{
-            Projectile.NewProjectile(position.X, position.Y, speedX, speedY, type, damage, knockBack, player.whoAmI);
+
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            Vector2 vector2 = player.RotatedRelativePoint(player.MountedCenter, true);
+            float num117 = 0.314159274f;
+            int num118 = 6;
+            Vector2 vector7 = new Vector2(speedX, speedY);
+            vector7.Normalize();
+            vector7 *= 40f;
+            bool flag11 = Collision.CanHit(vector2, 0, 0, vector2 + vector7, 0, 0);
+            for (int num119 = 0; num119 < num118; num119++)
+            {
+                float num120 = (float)num119 - ((float)num118 - 1f) / 2f;
+                Vector2 value9 = vector7.RotatedBy((double)(num117 * num120), default(Vector2));
+                if (!flag11)
+                {
+                    value9 -= vector7;
+                }
+                int num121 = Projectile.NewProjectile(vector2.X + value9.X, vector2.Y + value9.Y, speedX, speedY, type, (int)((double)damage), knockBack, player.whoAmI, 0.0f, 0.0f);
+                Main.projectile[num121].noDropItem = true;
+            }
+            float SpeedX = speedX + (float)Main.rand.Next(-25, 26) * 0.05f;
+            float SpeedY = speedY + (float)Main.rand.Next(-25, 26) * 0.05f;
+
             return false;
-		}
+
+        }
+
 
         public override void ModifyTooltips(List<TooltipLine> list)
         {
