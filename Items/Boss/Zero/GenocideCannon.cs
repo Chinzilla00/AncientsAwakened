7539bd85f7676b0f20945330593261e41a60ac00
GovernorGibs <52765683+GovernorGibs@users.noreply.github.com>
2019-07-29 16:37:48 -0700

    forgot some

--- Items/Boss/Zero/GenocideCannon.cs
+++ Items/Boss/Zero/GenocideCannon.cs
@@ -16,7 +16,7 @@ namespace AAMod.Items.Boss.Zero
 
         public override void SetDefaults()
         {
-            item.damage = 450;
+            item.damage = 400;
             item.ranged = true;
             item.width = 66;
             item.height = 28;
