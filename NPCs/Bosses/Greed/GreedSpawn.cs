f9d4299e985bec757d40727bb604e9f232f40a78
Alphakip <38961891+Alphakip@users.noreply.github.com>
2019-08-07 13:08:21 -0600

    Update GreedSpawn.cs

--- NPCs/Bosses/Greed/GreedSpawn.cs
+++ NPCs/Bosses/Greed/GreedSpawn.cs
@@ -4,7 +4,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 using Terraria.ModLoader;
 
-namespace AAMod.NPCs.Bosses.Yamata
+namespace AAMod.NPCs.Bosses.Greed
 {
     public class GreedSpawn : ModNPC
     {
