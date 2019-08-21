b8b0ce22d2d23a235076ea104c57f7095bee8d58
Alphakip <38961891+Alphakip@users.noreply.github.com>
2019-08-21 14:24:30 -0600

    Revert "Removed redundant abstract class"
    This reverts commit 4d663f7ab7a186309a867c51e7146f998dea06ff.

--- NPCs/Bosses/Greed/CoinRain.cs
+++ NPCs/Bosses/Greed/CoinRain.cs
@@ -1,38 +1,18 @@
-﻿using Microsoft.Xna.Framework;
-using Terraria;
-using Terraria.ID;
-using Terraria.ModLoader;
+﻿using Terraria.ID;
 
 namespace AAMod.NPCs.Bosses.Greed
 {
-    class CoinRain : ModProjectile
+    class CoinRain : FallingProjectile
     {
-        public override void SetStaticDefaults()
-        {
-            DisplayName.SetDefault("Covetite Coin");
-        }
+        public override string name => "Covetite Coin";
+        public override int Tile => TileID.GoldCoinPile;
+
 
         public override void SetDefaults()
         {
-            projectile.width = 14;
-            projectile.height = 14;
-            projectile.damage = 0;
-            projectile.ranged = true;
-            projectile.penetrate = 5;
-            projectile.tileCollide = true;
-            projectile.aiStyle = 10;
+            base.SetDefaults();
+            projectile.friendly = false;
             projectile.hostile = true;
-            aiType = ProjectileID.GoldCoinsFalling;
-        }
-
-        public override bool OnTileCollide(Vector2 oldVelocity)
-        {
-            return true;
-        }
-
-        public override void Kill(int timeLeft)
-        {
-            WorldGen.PlaceTile((int)(projectile.position.X / 16), (int)(projectile.position.Y / 16), TileID.GoldCoinPile);
         }
     }
 }
