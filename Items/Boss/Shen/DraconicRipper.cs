092a5efdf33727c1f43ca99b6f118012cf2d1958
TailstheFox92 <41931137+TailstheFox92@users.noreply.github.com>
2019-09-01 22:16:22 -0400

    Draconic Ripper Rework

--- Items/Boss/Shen/DraconicRipper.cs
+++ Items/Boss/Shen/DraconicRipper.cs
@@ -12,15 +12,14 @@ namespace AAMod.Items.Boss.Shen
 		{
 			item.useStyle = 5;
 			item.autoReuse = true;
-			item.useAnimation = 2;
-			item.useTime = 2;
+			item.useAnimation = 3;
+			item.useTime = 3;
 			item.width = 72;
-			item.height = 34;
-			item.shoot = 10;
-			item.useAmmo = AmmoID.Bullet;
+            item.height = 34;
+            item.shoot = ProjectileID.Bullet;
 			item.UseSound = SoundID.Item41;
 			item.damage = 100;
-			item.shootSpeed = 15f;
+			item.shootSpeed = 12f;
 			item.noMelee = true;
 			item.value = Item.sellPrice(0, 30, 0, 0);
 			item.rare = 11;
@@ -43,34 +42,38 @@ namespace AAMod.Items.Boss.Shen
         public override void SetStaticDefaults()
 		{
 			DisplayName.SetDefault("Draconic Ripper");
-			Tooltip.SetDefault("Shoots dozens of high-caliber bullets"
-			+"\nIgnores enemy defense"
-			+"\n77% chance not to consume ammo");
+			Tooltip.SetDefault("Shoots dozens of high-caliber teeth"
+			+"\nIgnores enemy defense");
         }
-
-		public override bool ConsumeAmmo(Player player)
-		{
-			return Main.rand.NextFloat() >= .77;
-		}
 		
 		public override Vector2? HoldoutOffset()
 		{
 			return new Vector2(-10, -2);
 		}
-		
-		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
-		{
-			Vector2 vector = player.MountedCenter;
-			int numberProjectiles = 2 + Main.rand.Next(3);
-			for (int i = 0; i < numberProjectiles; i++)
-			{
-				Vector2 perturbedSpeed = new Vector2(speedX, speedY).RotatedByRandom(MathHelper.ToRadians(10));
-				float scale = 1f - (Main.rand.NextFloat() * .4f);
-				perturbedSpeed *= scale; 
-				Projectile.NewProjectile(vector.X, vector.Y, perturbedSpeed.X, perturbedSpeed.Y, type, damage, knockBack, player.whoAmI);
-			}
-			return false;
-		}
+
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                string tooth;
+
+                if(i == 0)
+                {
+                    tooth = "ShenTooth";
+                }
+                else if(i == 1)
+                {
+                    tooth = "AkumaTooth";
+                }
+                else
+                {
+                    tooth = "YamataTooth";
+                }
+                Vector2 perturbedSpeed = new Vector2(speedX, speedY).RotatedByRandom(MathHelper.ToRadians(10));
+                Projectile.NewProjectile(position.X, position.Y, perturbedSpeed.X, perturbedSpeed.Y, mod.ProjectileType(tooth), damage, knockBack, player.whoAmI);
+            }
+            return false;
+        }
 
         public override void HoldItem(Player player)
 		{
---
+++ Projectiles/AkumaTooth.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+
+namespace AAMod.Projectiles
+{
+    public class AkumaTooth : ShenTooth
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Akuma Tooth");
+        }
+    }
+}
