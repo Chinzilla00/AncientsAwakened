d79265d0b764277e7b86ca855445a8c6a45c167a
Hallam9K <32880148+Hallam9K@users.noreply.github.com>
2019-10-02 21:10:18 +0100

    G R E E D

---
+++ Sounds/Custom/Quake1.cs
@@ -0,0 +1,17 @@
+using Microsoft.Xna.Framework.Audio;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace AAMod.Sounds.Custom
+{
+    public class Quake1 : ModSound
+    {
+        public override SoundEffectInstance PlaySound(ref SoundEffectInstance soundInstance, float volume, float pan, SoundType type)
+        {
+            soundInstance = sound.CreateInstance();
+            soundInstance.Volume = volume * 1f;
+            soundInstance.Pan = pan;
+            return soundInstance;
+        }
+    }
+}
