f99b99ac28d33f0c87b4c8c6f737a66390d993ef
GovernorGibs <52765683+GovernorGibs@users.noreply.github.com>
2019-11-09 23:08:01 -0800

    random solar nerf

--- Items/Boss/Akuma/Solar.cs
+++ Items/Boss/Akuma/Solar.cs
@@ -21,7 +21,7 @@ Inflicts daybroken");
         public override void SetDefaults()
         {
             item.CloneDefaults(ItemID.Terrarian);
-            item.damage = 750;                            
+            item.damage = 300;                            
             item.value = Item.sellPrice(0, 30, 0, 0);
             item.rare = 2;
             item.knockBack = 1;
