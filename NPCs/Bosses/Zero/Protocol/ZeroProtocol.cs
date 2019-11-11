f8f16779fe41ca3706064069b85fcf993a440124
Cerberus <51979790+tyhnoone@users.noreply.github.com>
2019-11-11 18:05:59 +0800

    Update ZeroProtocol.cs

--- NPCs/Bosses/Zero/Protocol/ZeroProtocol.cs
+++ NPCs/Bosses/Zero/Protocol/ZeroProtocol.cs
@@ -510,7 +510,7 @@ namespace AAMod.NPCs.Bosses.Zero.Protocol
                             }
                             
                             if (Main.netMode != 1) AAMod.Chat(@"===C A R N A G E===", Color.Red.R, Color.Red.G, Color.Red.B);
-                            Projectile.NewProjectile(npc.position, 10f * npc.DirectionTo(player.Center), ModContent.ProjectileType<EchoRay>(), npc.damage/2, 3f, Main.myPlayer, 0, npc.whoAmI);
+                            Projectile.NewProjectile(npc.position, 10f * npc.DirectionTo(player.Center), ModContent.ProjectileType<EchoRay>(), 100, 3f, Main.myPlayer, 0, npc.whoAmI);
                             npc.ai[3] = 1f;
                         }
                         else
