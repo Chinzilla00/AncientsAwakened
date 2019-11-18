492b5695777235b3daf84ae5829c631fc7c03e3e
Cerberus <51979790+tyhnoone@users.noreply.github.com>
2019-11-19 01:02:12 +0800

    Update ZeroMini.cs

--- NPCs/Bosses/Zero/Protocol/ZeroMini.cs
+++ NPCs/Bosses/Zero/Protocol/ZeroMini.cs
@@ -39,7 +39,7 @@ namespace AAMod.NPCs.Bosses.Zero.Protocol
 
         public override void ScaleExpertStats(int numPlayers, float bossLifeScale)
         {
-            npc.lifeMax = (int)(npc.lifeMax * 0.5f);
+            npc.lifeMax = (int)(npc.lifeMax * 0.8f);
         }
 
         public override Color? GetAlpha(Color drawColor)
