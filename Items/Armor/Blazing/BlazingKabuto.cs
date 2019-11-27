35c9e7d6d472be7033d6708e0a396152bb96c334
GovernorGibs <52765683+GovernorGibs@users.noreply.github.com>
2019-11-27 14:18:32 -0800

    tooltips

--- Items/Armor/Blazing/BlazingKabuto.cs
+++ Items/Armor/Blazing/BlazingKabuto.cs
@@ -12,7 +12,7 @@ namespace AAMod.Items.Armor.Blazing
 		public override void SetStaticDefaults()
 		{
 			DisplayName.SetDefault("Blazing Kabuto");
-			Tooltip.SetDefault(@"2% increased Damage Resistance
+			Tooltip.SetDefault(@"1% increased Damage Resistance
 Forged in the flames of the blazing sun");
         }
 
