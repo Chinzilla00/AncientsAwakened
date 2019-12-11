0a1b81e1fee56357f67ef1c828c00577b36d6b74
Alphakip <38961891+Alphakip@users.noreply.github.com>
2019-12-10 22:42:42 -0700

    Update Cerberus.cs

--- Items/Pets/Cerberus.cs
+++ Items/Pets/Cerberus.cs
@@ -45,20 +45,13 @@ namespace AAMod.Items.Pets
 
         public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
         {
-            Vector2 mountedCenter = Main.player[projectile.owner].MountedCenter;
-            Color color25 = Lighting.GetColor((int)(projectile.position.X + projectile.width * 0.5) / 16, (int)((projectile.position.Y + projectile.height * 0.5) / 16.0));
-
-            int num147 = 8;
-            int num148 = -18;
-
             float num149 = (Main.projectileTexture[projectile.type].Width - projectile.width) * 0.5f + projectile.width * 0.5f;
 
             SpriteEffects spriteEffects = projectile.spriteDirection == -1 ? SpriteEffects.FlipHorizontally : SpriteEffects.None;
 
-            int num312 = Main.projectileTexture[projectile.type].Height / 11;
-            int y15 = num312 * projectile.frame;
+            int y15 = 40 * projectile.frame;
 
-            Main.spriteBatch.Draw(Main.projectileTexture[projectile.type], new Vector2(projectile.position.X - Main.screenPosition.X + num149 + num148, projectile.position.Y - Main.screenPosition.Y + projectile.height / 2 + projectile.gfxOffY), new Rectangle?(new Rectangle(0, y15, Main.projectileTexture[projectile.type].Width, num312 - 1)), projectile.GetAlpha(color25), projectile.rotation, new Vector2(num149, projectile.height / 2 + num147), projectile.scale, spriteEffects, 0f);
+            Main.spriteBatch.Draw(Main.projectileTexture[projectile.type], new Vector2(projectile.position.X - Main.screenPosition.X + num149 + -18, projectile.position.Y - Main.screenPosition.Y + projectile.height / 2 + projectile.gfxOffY), new Rectangle?(new Rectangle(0, y15, Main.projectileTexture[projectile.type].Width, 40 - 1)), projectile.GetAlpha(color25), projectile.rotation, new Vector2(num149, projectile.height / 2 + 8), projectile.scale, spriteEffects, 0f);
 
             return false;
         }
