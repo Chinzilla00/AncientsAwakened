6d5898de4d02992f34ff7bfe1975b3db633f2541
Alphakip <38961891+Alphakip@users.noreply.github.com>
2020-01-15 12:19:56 -0700

    Create SwampKoi.cs

---
+++ Items/Materials/SwampKoi.cs
@@ -0,0 +1,17 @@
+namespace AAMod.Items.Materials
+{
+    public class SwampKoi : BaseAAItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Swamp Koi");
+        }
+        public override void SetDefaults()
+        {
+            item.width = 22;
+            item.height = 22;
+            item.maxStack = 99;
+            item.rare = 3;
+        }
+    }
+}
