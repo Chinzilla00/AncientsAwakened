fdb4f888dd2718334a46bf99a9ea4dd52bbc8800
BlazenBreaker <53154367+BlazenBreaker@users.noreply.github.com>
2020-03-24 16:13:42 -0400

    Added the dark matter slasher (item and slashes)
    an weapon that takes advantage of the saber code and merely an test item to test the waters of this new code it may be a bit unbalanced but I tried my best and this is what I got an post equinox weapon

---
+++ Items/Melee/DMSlasher.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Localization;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace AAMod.Items.Melee
+{
+    public class DMSlasher : BaseAAItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("DarkMatter Slasher");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 56;
+            item.height = 56;
+            item.melee = true;
+            item.damage = 350; 
+            item.knockBack = 3;
+            item.autoReuse = true;
+            item.useStyle = 1;
+            item.UseSound = SoundID.Item1;
+            item.useTime = 30 / 2; //dont change the 30 unless you want to soft lock your computer. instead use it as a value and use 15 as the number 2, doing this will divide the 30 with 15 to get a use time of 2
+            item.useAnimation = 13;
+            item.shoot = mod.ProjectileType("MS");
+            item.shootSpeed = 25f;
+            item.rare = 2;
+            item.value = 25000;
+        }
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(null, "DarkEnergy", 35);
+            recipe.AddIngredient(null, "DarkMatter", 25);
+		    recipe.AddTile(null, "QuantumFusionAccelerator");
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+
+        public override void HoldItem(Player player)
+        {
+            Saber.HoldItemManager(player, item, ModContent.ProjectileType<DMSlash>(),
+                Color.Blue, 0.9f, player.itemTime == 0 ? 0f : 1f);
+        }
+
+        // Doesn't get called unless item.shoot is defined.
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        { return Saber.IsChargedShot(player); }
+
+        public override bool UseItemFrame(Player player)
+        {
+            Saber.UseItemFrame(player, 0.9f, item.isBeingGrabbed);
+            return true;
+        }
+
+        public override void UseItemHitbox(Player player, ref Rectangle hitbox, ref bool noHitbox)
+        {
+            int height = 64;
+            int length = 80;
+            Saber.UseItemHitboxCalculate(player, item, ref hitbox, ref noHitbox, 0.9f, height, length);
+        }
+
+        public override void OnHitNPC(Player player, NPC target, int damage, float knockBack, bool crit)
+        {
+            Color colour = new Color(0.1f, 255f, 181f);
+            Saber.OnHitFX(player, target, crit, colour, true);
+        }
+
+        public override void ModifyHitNPC(Player player, NPC target, ref int damage, ref float knockBack, ref bool crit)
+        {
+            if (Saber.SabreIsChargedStriking(player, item))
+            { damage = -500; }
+        }
+    }
+    public class DMSlash : ModProjectile
+    {
+        public static Texture2D specialSlash;
+        public static int specialProjFrames = 7;
+        bool sndOnce = true;
+        int chargeSlashDirection = 1;
+
+
+        public override void SetStaticDefaults()
+        {
+            Main.projFrames[projectile.type] = 7;
+            if (Main.netMode == 2) return;
+            specialSlash = mod.GetTexture("Items/Melee/" + GetType().Name + "2");
+        }
+        public override void SetDefaults()
+        {
+            projectile.width = 120;
+            projectile.height = 80;
+            projectile.aiStyle = -1;
+            projectile.timeLeft = 60;
+            projectile.friendly = true;
+            projectile.melee = true;
+            projectile.tileCollide = false;
+            projectile.ignoreWater = true;
+        }
+        public override bool? CanCutTiles() { return false; }
+        public int FrameCheck
+        {
+            get { return (int)projectile.ai[0]; }
+            set { projectile.ai[0] = value; }
+        }
+        public int SlashLogic
+        {
+            get { return (int)projectile.ai[1]; }
+            set { projectile.ai[1] = value; }
+        }
+
+        public override void AI()
+        {
+            Player player = Main.player[projectile.owner];
+            if (Saber.AINormalSlash(projectile, SlashLogic)) { }
+            else
+            {
+                // Charged attack
+                Saber.AISetChargeSlashVariables(player, chargeSlashDirection);
+                Saber.NormalSlash(projectile, player);
+            }
+            projectile.damage = 0;
+            projectile.ai[0] += 1f; // Framerate
+        }
+
+
+        public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
+        {
+            Player player = Main.player[projectile.owner];
+            int weaponItemID = ModContent.ItemType<DMSlasher>();
+            Color lighting = Lighting.GetColor((int)(player.MountedCenter.X / 16), (int)(player.MountedCenter.Y / 16));
+            return Saber.PreDrawSlashAndWeapon(spriteBatch, projectile, weaponItemID, lighting,
+                SlashLogic == 0f ? specialSlash : null,
+                SlashLogic == 0f ? new Color(1f, 255f, 181f, 1f) : lighting, 
+                specialProjFrames,
+                SlashLogic == 0f ? chargeSlashDirection : SlashLogic);
+        }
+
+    }
+}
+
