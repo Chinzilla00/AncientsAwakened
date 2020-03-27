9e79d45e05d118224ab99677bbfd756548b54638
Alphakip <38961891+Alphakip@users.noreply.github.com>
2020-03-27 13:27:58 -0600

    New Anubis weap

---
+++ Items/Boss/Anubis/Forsaken/SoulCrook.cs
@@ -0,0 +1,60 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using BaseMod;
+
+namespace AAMod.Items.Boss.Anubis.Forsaken
+{
+	public class SoulCrook : BaseAAItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Crook of the Soul Judge");	
+            BaseUtility.AddTooltips(item, new string[] { "Phases through tiles", "Every hit the crook makes heals you when it returns" });			
+		}
+
+        public override void SetDefaults()
+        {
+            item.width = 50;
+            item.height = 50;
+            item.maxStack = 5;
+            item.useStyle = 1;
+            item.useAnimation = 15;
+            item.useTime = 15;
+            item.UseSound = SoundID.Item1;
+            item.damage = 160;
+            item.knockBack = 10;
+            item.melee = true;
+            item.autoReuse = true;
+            item.noUseGraphic = true;
+            item.noMelee = true;
+            item.shoot = mod.ProjType("Crook");
+            item.shootSpeed = 15;
+            item.rare = 9;
+            AARarity = 12;
+        }
+
+        public override void ModifyTooltips(System.Collections.Generic.List<Terraria.ModLoader.TooltipLine> list)
+        {
+            foreach (Terraria.ModLoader.TooltipLine line2 in list)
+            {
+                if (line2.mod == "Terraria" && line2.Name == "ItemName")
+                {
+                    line2.overrideColor = AAColor.Rarity12;
+                }
+            }
+        }
+
+        public override bool CanUseItem(Player player)       //this make that you can shoot only 1 boomerang at once
+        {
+            for (int i = 0; i < 1000; ++i)
+            {
+                if (Main.projectile[i].active && Main.projectile[i].owner == Main.myPlayer && Main.projectile[i].type == item.shoot)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+	}
+}
--- Projectiles/Anubis/Forsaken/Crook.cs
+++ Projectiles/Anubis/Forsaken/Crook.cs
@@ -1,6 +1,7 @@
 using BaseMod;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using System;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -20,12 +21,16 @@ namespace AAMod.Projectiles.Anubis.Forsaken
             projectile.penetrate = -1;
         }
 
+        int HealAmt = 0;
+
 		public override void AI()
 		{
-            if (Main.rand.Next(2) == 0)
+            Player player = Main.player[projectile.owner];
+            for (int a = 0; a < HealAmt; a++)
             {
-                int dustnumber = Dust.NewDust(projectile.position, projectile.width, projectile.height, ModContent.DustType<Dusts.ForsakenDust>(), 0f, 0f, 200, default, 0.5f);
+                int dustnumber = Dust.NewDust(projectile.Center, 0, 0, ModContent.DustType<Dusts.ForsakenDust>(), 0f, 0f, 100, default, 0);
                 Main.dust[dustnumber].velocity *= 0.3f;
+                Main.dust[dustnumber].noGravity = true; ;
             }
 
             for (int m = projectile.oldPos.Length - 1; m > 0; m--)
@@ -34,15 +39,37 @@ namespace AAMod.Projectiles.Anubis.Forsaken
             }
             projectile.oldPos[0] = projectile.position;
 
-            BaseAI.AIBoomerang(projectile, ref projectile.ai, Main.npc[master].position, Main.npc[master].width, Main.npc[master].height, true, 40, 45, 10f, 1f, true);
+            Vector2 vector36 = new Vector2(projectile.position.X + projectile.width * 0.5f, projectile.position.Y + projectile.height * 0.5f);
+            float num489 = player.Center.X - vector36.X;
+            float num490 = player.Center.Y - vector36.Y;
+            float num491 = (float)Math.Sqrt(num489 * num489 + num490 * num490);
+            if (num491 < 50f && projectile.position.X < player.position.X + player.width && projectile.position.X + projectile.width > player.position.X && projectile.position.Y < player.position.Y + player.height && projectile.position.Y + projectile.height > player.position.Y)
+            {
+                if (projectile.owner == Main.myPlayer)
+                {
+                    player.HealEffect(HealAmt, false);
+                    player.statLife += 1;
+                    if (player.statLife > player.statLifeMax2)
+                    {
+                        player.statLife = player.statLifeMax2;
+                    }
+                    NetMessage.SendData(66, -1, -1, null, projectile.owner, 1, 0f, 0f, 0, 0, 0);
+                }
+                projectile.Kill();
+            }
+
+            BaseAI.AIBoomerang(projectile, ref projectile.ai, player.position, player.width, player.height, true, 40, 45, 10, .6f, true);
+        }
+
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            HealAmt++;
         }
 
         public override bool PreDraw(SpriteBatch sb, Color dColor)
         {
             Rectangle frame = BaseDrawing.GetFrame(projectile.frame, Main.projectileTexture[projectile.type].Width, Main.projectileTexture[projectile.type].Height, 0, 0);
-
             BaseDrawing.DrawAfterimage(sb, Main.projectileTexture[projectile.type], 0, projectile, 2f, 1f, 5, true, 0f, 0f, dColor);
-
             BaseDrawing.DrawTexture(sb, Main.projectileTexture[projectile.type], 0, projectile.position, projectile.width, projectile.height, projectile.scale, projectile.rotation, 0, 1, frame, dColor, true);
             return false;
         }
