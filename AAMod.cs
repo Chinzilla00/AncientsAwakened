faebc59b3f2c64a833e9f0d693ece99dfdd0e1e3
Alphakip <38961891+Alphakip@users.noreply.github.com>
2020-03-29 15:07:04 -0600

    Update AAMod.cs

--- AAMod.cs
+++ AAMod.cs
@@ -330,9 +330,6 @@ namespace AAMod
             PremultiplyTexture(GetTexture("Projectiles/RadiumStar"));
             PremultiplyTexture(GetTexture("Projectiles/Stars"));
             PremultiplyTexture(GetTexture("NPCs/Bosses/Toad/ToadBubble"));
-            PremultiplyTexture(GetTexture("NPCs/Bosses/Greed/GreedSpawn"));
-            PremultiplyTexture(GetTexture("NPCs/Bosses/Greed/GreedSpawn1"));
-            PremultiplyTexture(GetTexture("NPCs/Bosses/Greed/GreedSpawn2"));
             PremultiplyTexture(GetTexture("NPCs/Bosses/Zero/Protocol/ProtoStar"));
 
             if (GetSoundSlot(SoundType.Music, "Sounds/Music/Monarch") != 0) //ensure music was loaded!
