53c54682e77c847a845651bc109fa37e459a0fbd
Alphakip <38961891+Alphakip@users.noreply.github.com>
2020-04-12 10:11:11 -0600

    Update StarAltar.cs

--- Tiles/Altar/StarAltar.cs
+++ Tiles/Altar/StarAltar.cs
@@ -84,7 +84,7 @@ namespace AAMod.Tiles.Altar
         public override bool NewRightClick(int i, int j)
         {
             Player player = Main.LocalPlayer;
-            int type = ModContent.ItemType<Items.Boss.Athena.StarChart>();
+            int type = ModContent.ItemType<Items.Boss.Athena.Olympian.StarChart>();
             if (BasePlayer.HasItem(player, type, 1) && !AAWorld.StarActive)
             {
                 for (int m = 0; m < 50; m++)
