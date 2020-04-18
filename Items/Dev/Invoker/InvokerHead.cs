2199a76d4ec6b73ac41c555eb27e946ce75cce6e
Cerberus <51979790+tyhnoone@users.noreply.github.com>
2020-04-18 08:17:16 +0800

    Update InvokerHead.cs

--- Items/Dev/Invoker/InvokerHead.cs
+++ Items/Dev/Invoker/InvokerHead.cs
@@ -2,12 +2,12 @@ using Terraria.ModLoader;
 
 namespace AAMod.Items.Dev.Invoker
 {
-    [AutoloadEquip(EquipType.Legs)]
+    [AutoloadEquip(EquipType.Head)]
 	public class InvokerHead : EquipTexture
 	{
 		public override bool DrawHead()
 		{
-			return false;
+			return true;
 		}
 	}
 }
