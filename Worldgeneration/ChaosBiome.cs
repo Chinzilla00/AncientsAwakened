ea179d08072f26ea40f13572069f61362b03d5a0
Alphakip <38961891+Alphakip@users.noreply.github.com>
2020-05-01 12:57:35 -0600

    Update ChaosBiome.cs

--- Worldgeneration/ChaosBiome.cs
+++ Worldgeneration/ChaosBiome.cs
@@ -1417,6 +1417,7 @@ namespace AAMod.Worldgeneration
             }
 
             WorldGen.PlaceTile(origin.X + 33, origin.Y + 137, mod.TileType("InvokerBookTile"), true, false);
+            WorldGen.PlaceTile(origin.X + 238, origin.Y + 104, mod.TileType("TerraDresser"), true, false);
 
             return true;
         }
